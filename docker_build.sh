a8d094a01e35312abc39912a62a559cdd1079aa6
tkrikoriam <tony.krikorian@outlook.com>
2020-09-29 16:40:11 -0300

    Add v1 backend

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,9 @@
 #Execute docker build 
-sudo docker build -t tkrikoriam/translation-microservice:v11 . 
- \ --build-arg value7000="987001e00cf5495aa064b9212cddf659" 
-  \ --build-arg urltokenazure="https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken" 
-   \ --build-arg azuretraductorurl="https://api.cognitive.microsofttranslator.com"
-   \ --build-arg languageslist="https://api.cognitive.microsofttranslator.com/languages?api-version=3.0"
+sudo docker build -t tkrikoriam/translation-microservice:v11 . \
+   --build-arg value7000="987001e00cf5495aa064b9212cddf659" \
+   --build-arg urltokenazure="https://eastus.api.cognitive.microsoft.com/sts/v1.0/issueToken" \
+   --build-arg azuretraductorurl="https://api.cognitive.microsofttranslator.com" \
+   --build-arg languageslist="https://api.cognitive.microsofttranslator.com/languages?api-version=3.0" \
 
 #Docker run
 docker run -d -p3500:80 --name translation-microservice tkrikoriam/translation-microservice:v11
--- upload_docker.sh
+++ upload_docker.sh
@@ -2,4 +2,4 @@
 docker login
 
 #Subir la imagen
-docker push tkrikoriam/translation-microservice:v10
+docker push tkrikoriam/translation-microservice:v11
