1f564ac84853e9e4cb60486142e8bd1b9d4edb3b
tkrikoriam <tony.krikorian@outlook.com>
2020-09-29 17:20:23 -0300

    Add kubernetes files

---
+++ Kubernetes/kubernetes_apply.sh
@@ -0,0 +1,7 @@
+kubectl apply -f backend-secret.yml
+kubectl apply -f backend-configmap.yml
+kubectl apply -f backend-deployment.yml
+kubectl apply -f backend-external-service.yml
+kubectl get pods -n translation-microservice
+
+
