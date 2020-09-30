e99911a3b0d41f336ba6259a0f387af0f7242ccd
tkrikoriam <tony.krikorian@outlook.com>
2020-09-30 16:38:11 -0300

    Add docker external service

---
+++ Kubernetes/backend/kubernetes_apply.sh
@@ -0,0 +1,11 @@
+kubectl apply -f backend-namespace.yml
+kubectl apply -f backend-secret.yml
+kubectl apply -f backend-configmap.yml
+kubectl apply -f backend-deployment.yml
+kubectl apply -f backend-external-service.yml
+#kubectl apply -f backend-ingress.yml
+
+kubectl get all -n microservices
+
+
+
