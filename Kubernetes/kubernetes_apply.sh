715c72526b155f3755fbc12fa2c84a98b60916e2
tkrikoriam <tony.krikorian@outlook.com>
2020-10-01 06:23:51 -0300

    Add kubernetes ports

--- Kubernetes/kubernetes_apply.sh
+++
@@ -1,8 +0,0 @@
-kubectl apply -f backend-namespace.yml
-kubectl apply -f backend-secret.yml
-kubectl apply -f backend-configmap.yml
-kubectl apply -f backend-deployment.yml
-kubectl apply -f backend-external-service.yml
-kubectl get pods -n translation-microservice
-
-
