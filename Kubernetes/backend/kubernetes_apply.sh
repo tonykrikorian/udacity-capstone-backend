ee105b8bb0e02656853419efbc6f6ea77fe39fb6
tkrikoriam <tony.krikorian@outlook.com>
2020-10-05 16:29:06 -0300

    Add deploy eks

--- Kubernetes/backend/kubernetes_apply.sh
+++ Kubernetes/backend/kubernetes_apply.sh
@@ -3,7 +3,7 @@ kubectl apply -f backend-secret.yml
 kubectl apply -f backend-configmap.yml
 kubectl apply -f backend-deployment.yml
 kubectl apply -f backend-external-service.yml
-#kubectl apply -f backend-ingress.yml
+kubectl apply -f backend-ingress.yml
 
 kubectl get all -n microservices
 
