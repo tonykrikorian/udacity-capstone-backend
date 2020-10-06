ea394f1b8efe2ec05b5a4516a24444cee25f8d3c
tkrikoriam <tony.krikorian@outlook.com>
2020-10-06 07:05:04 -0300

    enforce airbnb js style

--- routes/translate.js
+++ routes/translate.js
@@ -1,19 +1,20 @@
-const Router = require("express-promise-router");
-const translationService = require("../services/translation");
-const getLanguageListService = require("../services/languagesList");
+const Router = require('express-promise-router');
+const translationService = require('../services/translation');
+const getLanguageListService = require('../services/languagesList');
+
 const route = new Router();
 
-route.post("/", async (req, res) => {
+route.post('/', async (req, res) => {
   const { message, fromLanguage, toLanguage } = req.body;
   const traducciones = await translationService(
     message,
     fromLanguage,
-    toLanguage
+    toLanguage,
   );
   return res.send(traducciones);
 });
 
-route.get("/languages", async (req, res) => {
+route.get('/languages', async (req, res) => {
   const response = await getLanguageListService();
   return res.send(response);
 });
